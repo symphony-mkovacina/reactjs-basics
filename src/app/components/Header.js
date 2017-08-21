86a3d359e63c0c91077d5e1bc8b229597132a183
Marko Kovacina <marko.kovacina@symphony.is>
2017-08-22 00:29:36 +0200

    Routing: Navigation & Parameters

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export const Header = (props) => {
     return (
@@ -6,8 +7,8 @@ export const Header = (props) => {
             <div className="container">
                 <div className="navbar-header">
                     <ul className="nav navbar-nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">User</a></li>
+                        <li><Link to={"/home"}>Home</Link></li>
+                        <li><Link to={"/user/10"}>User</Link></li>
                     </ul>
                 </div>
             </div>
--- src/app/index.js
+++ src/app/index.js
@@ -12,7 +12,7 @@ class App extends React.Component {
             <Router history={browserHistory}>
                 <Route path={"/"} component={Root}>
                     <IndexRoute component={Home}/>
-                    <Route path={"user"} component={User}/>
+                    <Route path={"user/:id"} component={User}/>
                     <Route path={"home"} component={Home}/>
                 </Route>
             </Router>
